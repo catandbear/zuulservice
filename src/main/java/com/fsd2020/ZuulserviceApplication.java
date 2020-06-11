13c21fa831ed1434132fa0379a76e0cceee62107
Yi Zheng Cao <yzcaodl@cn.ibm.com>
2020-06-11 12:53:58 +0800

    commit zuul changes

--- src/main/java/com/fsd2020/ZuulserviceApplication.java
+++ src/main/java/com/fsd2020/ZuulserviceApplication.java
@@ -2,8 +2,10 @@ package com.fsd2020;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ZuulserviceApplication {
 
 	public static void main(String[] args) {
