7051c7aec16c0b9715e7d01afee0b213e12363bc
Yi Zheng Cao <yzcaodl@cn.ibm.com>
2020-06-12 15:58:11 +0800

    commit

--- src/test/java/com/fsd2020/ZuulserviceApplicationTests.java
+++ src/test/java/com/fsd2020/ZuulserviceApplicationTests.java
@@ -1,12 +1,7 @@
 package com.fsd2020;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class ZuulserviceApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
